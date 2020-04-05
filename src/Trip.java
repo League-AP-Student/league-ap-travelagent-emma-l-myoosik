63223b87255e116f9e77580cd49fe59f975c9515
emma-l-myoosik <60460092+emma-l-myoosik@users.noreply.github.com>
2020-04-05 14:35:35 -0700

    almost finished

--- src/Trip.java
+++ src/Trip.java
@@ -4,12 +4,30 @@ public class Trip {
 	private List<Flight> flights;
 
 	public int getDuration() {
-		int duration = 0;
-		return duration;
+		if (flights.size() == 0) {return 0;}
+		
+		Time departure = flights.get(0).getDeparturetime();
+		Time arrival = flights.get(flights.size() - 1).getArrivalTime();
+		return departure.minutesUntil(arrival);
 	}
 
 	public int getShortestLayover() {
-		int shortest = -1;
+		if (flights.size() < 2) { return -1; }
+		
+		int shortest = getDuration();
+		for (int i = 0; i < flights.size() - 1; i++) {
+			Flight f1 = flights.get(i);
+			Flight f2 = flights.get(i + 1);
+			
+			Time arrival = f1.getArrivalTime();
+			Time departure = f2.getDeparturetime();
+			
+			int layover = arrival.minutesUntil(departure);
+			if (layover < shortest) {
+				shortest = layover;
+			}
+			
+		}
 		return shortest;
 	}
 }
